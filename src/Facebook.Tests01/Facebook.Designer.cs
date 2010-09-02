9f9100103bcbddb526c76f6b57f012a8e94a85c9
ntotten <none@none>
2010-09-02 19:15:49 +0000

    Added back updated Facebook.Tests project.
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf/Main%4054803

---
+++ src/Facebook.Tests01/Facebook.Designer.cs
@@ -0,0 +1,2 @@
+// <assemblyHash>c44b06f5</assemblyHash>
+// <compilationHash>3xxjZvBTTQq0ef8mImzPfTZD7hE=C8zf5gSTf+mOo0GU5AB1QvopFAM=</compilationHash>
---
+++ src/Facebook.Tests01/Integration/RestReadTests.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------
+// <copyright file="RestReadTests.cs" company="Thuzi, LLC">
+//     Copyright (c) 2010 Thuzi, LLC (thuzi.com)
+// </copyright>
+// <author>Nathan Totten (ntotten.com) and Jim Zimmerman (jimzimmerman.com)</author>
+// <license>Released under the terms of the Microsoft Public License (Ms-PL)</license>
+// <website>http://facebooksdk.codeplex.com</website>
+// ---------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Dynamic;
+
+namespace Facebook.Tests.Rest {
+    [TestClass]
+    public class RestReadTests {
+
+        [TestMethod]
+        public void user_getInfo_rest_should_throw_oauth()
+        {
+            try
+            {
+                FacebookApp app = new FacebookApp();
+
+                dynamic parameters = new ExpandoObject();
+                parameters.method = "user.getInfo";
+                parameters.uids = "14812017";
+                parameters.fields = "first_name,last_name";
+                parameters.access_token = "invalidtoken";
+
+                var result = app.Api(parameters);
+
+                var firstName = result[0].first_name;
+                Assert.Fail(); // Should have thown by now
+            }
+            catch (FacebookOAuthException)
+            {
+                // Correct exception
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+        
+
+    }
+}
