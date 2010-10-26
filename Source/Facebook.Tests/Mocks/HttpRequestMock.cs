72f6e5997149e1c1df912ea177f950d4c6a2d09f
ntotten <none@none>
2010-10-25 23:43:31 -0400

    - Added Http mocks

---
+++ Source/Facebook.Tests/Mocks/HttpRequestMock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Facebook.Tests.Mocks
+{
+    public class HttpRequestMock : HttpRequestBase
+    {
+
+        public override Uri Url
+        {
+            get
+            {
+                return base.Url;
+            }
+        }
+
+        public override System.Collections.Specialized.NameValueCollection Headers
+        {
+            get
+            {
+                return base.Headers;
+            }
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return base.Cookies;
+            }
+        }
+
+        public override System.Collections.Specialized.NameValueCollection Params
+        {
+            get
+            {
+                return base.Params;
+            }
+        }
+
+    }
+}
