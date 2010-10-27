2ab603f6c42cc2fd015c1fd75d82c6e454cf96b7
ntotten <none@none>
2010-10-27 09:58:09 -0400

    - Added missing files

---
+++ Source/Facebook.TestUtils/Mocks/HttpRequestMock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace Facebook.Tests.Mocks
+{
+    public class HttpRequestMock : HttpRequestBase
+    {
+        private Uri url;
+
+        public HttpRequestMock(Uri url)
+        {
+            this.url = url;
+        }
+
+        public override Uri Url
+        {
+            get
+            {
+                return this.url;
+            }
+        }
+
+        public override System.Collections.Specialized.NameValueCollection Headers
+        {
+            get
+            {
+                return base.Headers;
+            }
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return base.Cookies;
+            }
+        }
+
+        public override System.Collections.Specialized.NameValueCollection Params
+        {
+            get
+            {
+                return base.Params;
+            }
+        }
+
+    }
+}
---
+++ Source/Facebook.TestUtils/Mocks/HttpResponseMock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Facebook.Tests.Mocks
+{
+    public class HttpResponseMock : HttpResponseBase
+    {
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return base.Cookies;
+            }
+        }
+
+    }
+}
