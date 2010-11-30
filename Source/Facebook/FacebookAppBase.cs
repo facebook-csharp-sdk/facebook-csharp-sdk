f1fade8d8025b16d50edf716b831b062ffb47b26
prabirshrestha <mail@prabir.me>
2010-11-30 22:20:24 +0700

    - Hide System.Object inherited methods from FacebookAppBase

--- Source/Facebook/FacebookAppBase.cs
+++ Source/Facebook/FacebookAppBase.cs
@@ -12,6 +12,7 @@ namespace Facebook
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
     using System.Diagnostics.Contracts;
     using System.Linq;
 
@@ -1322,5 +1323,55 @@ namespace Facebook
 
         #endregion
 
+        #region Hide System.Object inherited methods
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>. </param><filterpriority>2</filterpriority>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+#pragma warning disable 0108
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public Type GetType()
+        {
+            return base.GetType();
+        }
+#pragma warning restore 0108
+
+        /// <summary>
+        /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
     }
 }
