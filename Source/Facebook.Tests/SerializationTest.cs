99efb77e0376621d86b64e64dc98635b3fb9d62a
prabirshrestha <mail@prabir.me>
2010-11-30 23:49:06 +0700

    + converted SerializationTest.Fql_Query_Serialization to xunit

--- Source/Facebook.Tests/SerializationTest.cs
+++ Source/Facebook.Tests/SerializationTest.cs
@@ -26,15 +26,15 @@ namespace Facebook.Tests
             Assert.AreEqual("method=stream.publish&message=my%20message&attachment=%7B%22name%22%3A%22my%20attachment%22%2C%22href%22%3A%22http%3A%2F%2Fapps.facebook.com%2Fcanvas%22%7D", result);
         }
 
-        [TestMethod]
-        public void Fql_Query_Serialization()
+        [Fact(DisplayName = "ToJsonQueryString: Given a FQL query Should serialize it correctly")]
+        public void ToJsonQueryString_GivenAFQLQuery_ShouldSerializeItCorrectly()
         {
             dynamic parameters = new ExpandoObject();
             parameters.method = "stream.publish";
             parameters.query = "SELECT metric, value FROM insights WHERE object_id=111605505520003 AND end_time=1272276000 AND period=86400 AND metric='page_like_adds'";
 
             string result = DictionaryUtilities.ToJsonQueryString(parameters);
-            Assert.AreEqual("method=stream.publish&query=SELECT%20metric%2C%20value%20FROM%20insights%20WHERE%20object_id%3D111605505520003%20AND%20end_time%3D1272276000%20AND%20period%3D86400%20AND%20metric%3D'page_like_adds'", result);
+            Xunit.Assert.Equal("method=stream.publish&query=SELECT%20metric%2C%20value%20FROM%20insights%20WHERE%20object_id%3D111605505520003%20AND%20end_time%3D1272276000%20AND%20period%3D86400%20AND%20metric%3D'page_like_adds'", result);
         }
 
         [Fact(DisplayName = "SerializeObject: Given a DateTime with time zone Should serialize it correctly to ISO-8601 date time format")]
