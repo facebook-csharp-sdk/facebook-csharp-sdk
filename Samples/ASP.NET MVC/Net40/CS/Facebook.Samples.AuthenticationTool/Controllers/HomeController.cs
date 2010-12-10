4698438166ac771a6add631cf5027ce30986d932
DevTheo <none@none>
2010-12-10 12:18:42 -0500

    Renamed and moved some files around for tge Authentication MVC sample

---
+++ Samples/ASP.NET MVC/Net40/CS/Facebook.Samples.AuthenticationTool/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using Facebook;
+using Facebook.Web.Mvc;
+
+namespace Auth_And_Allow.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        [CanvasAuthorize(Perms = "user_about_me")]
+        public ActionResult Index()
+        {
+            FacebookApp fbApp = new FacebookApp();
+            if (fbApp.Session != null)
+            {
+                dynamic result = fbApp.Get("me");
+
+                ViewData["Firstname"] = result.first_name;
+                ViewData["Lastname"] = result.last_name;
+            }
+
+            return View();
+        }
+    }
+}
