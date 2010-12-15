95facec3e840a17a8b8f1a0e31740b6566fb404d
prabirshrestha <mail@prabir.me>
2010-12-15 23:27:33 +0700

    + FacebookAuthenticationResult Tests

---
+++ Source/Facebook.Tests/FacebookAuthenticationResultTests.cs
@@ -0,0 +1,67 @@
+namespace Facebook.Tests
+{
+    using Xunit;
+
+    public class FacebookAuthenticationResultTests
+    {
+        [Fact(DisplayName = "Parse: Given a url containing access_token and expires_in fragment The result is successful")]
+        public void Parse_GivenAUrlContainingAccessTokenAndExpiresInFragment_TheResultIsSuccessful()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html#access_token=123|654aaaee068db-100001327642026|sd&expires_in=0";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact(DisplayName = "Parse: Given a url containing access_token and expires in fragment The access token of result is equal to the value passed in the access_token querystring")]
+        public void Parse_GivenAUrlContainingAccessTokenAndExpiresInFragment_TheAccessTokenOfResultIsEqualToTheValuePassedInTheAccessTokenQuerystring()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html#access_token=123|654aaaee068db-100001327642026|sd&expires_in=0";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.Equal("123|654aaaee068db-100001327642026|sd", result.AccessToken);
+        }
+
+        [Fact(DisplayName = "Parse: Given a url containing access_token and expires in fragment The ExpiresIn of result is equal to the value passed in the expires_in querystring")]
+        public void Parse_GivenAUrlContainingAccess_tokenAndExpiresInFragment_TheExpiresInOfResultIsEqualToTheValuePassedInTheExpiresInQuerystring()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html#access_token=123|654aaaee068db-100001327642026|sd&expires_in=0";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.Equal(0, result.ExpiresIn);
+        }
+
+        [Fact(DisplayName = "Parse: Given a url containing access_token and expires in fragment Then ErrorReasonText is null")]
+        public void Parse_GivenAUrlContainingAccess_tokenAndExpiresInFragment_ThenErrorReasonTextIsNull()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html#access_token=123|654aaaee068db-100001327642026|sd&expires_in=0";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.Null(result.ErrorReason);
+        }
+
+        [Fact(DisplayName = "Parse: Given a url containing error_reason querystring Then the result is unsuccessful")]
+        public void Parse_GivenAUrlContainingErrorReasonQuerystring_ThenTheResultIsUnsuccessful()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html?error_reason=user_denied&error=access_denied&error_description=The+user+denied+your+request.";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact(DisplayName = "Parse: Given a url containing error_reason querystring Then the ErrorReasonText is equal to the value of error_reason querystring")]
+        public void Parse_GivenAUrlContainingErrorReasonQuerystring_ThenTheErrorReasonTextIsEqualToTheValueOfError_reasonQuerystring()
+        {
+            var url = "http://www.facebook.com/connect/login_success.html?error_reason=user_denied&error=access_denied&error_description=The+user+denied+your+request.";
+
+            var result = FacebookAuthenticationResult.Parse(url);
+
+            Assert.Equal("user_denied", result.ErrorReason);
+        }
+    }
+}
