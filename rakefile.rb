6b301bf608744a9ff1f45e077cb3088759e4dbac
prabirshrestha <mail@prabir.me>
2011-01-03 12:26:32 +0700

    + rakefile :configure task

--- rakefile.rb
+++ rakefile.rb
@@ -1,1 +1,22 @@
 require File.join(File.dirname(__FILE__), 'Build/albacore/albacore.rb')
+
+PROJECT_NAME      = "Facebook C# SDK"
+PROJECT_NAME_SAFE = PROJECT_NAME
+LOG               = true                # TODO: enable albacore logging from ENV
+
+build_config = nil
+task :configure do
+    # do configuration stuffs here
+    Albacore.configure do |config|
+        config.log_level = :verbose if LOG
+    end
+    
+    root_path    = "#{File.dirname(__FILE__)}/"
+    base_version = 0
+    
+    build_config = {
+    }
+    
+end
+
+Rake::Task["configure"].invoke
