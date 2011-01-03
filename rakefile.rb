c184d51505f7e0f8cc46360b24b4dec21f28f7ef
prabirshrestha <mail@prabir.me>
2011-01-03 13:10:12 +0700

    + rakefile long version,nightly and ci build numbers

--- rakefile.rb
+++ rakefile.rb
@@ -28,31 +28,53 @@ task :configure do
             :tools  => "#{root_path}Tools/"
         },
         :version => {
-			:base		=> "#{base_version}"
+			:base =>   "#{base_version}",
+            :full => "#{base_version}",
+            :long => "#{base_version}"
 		},
         :vcs => { 				# version control system
-			:name   => '',		# version control name
-			:rev_id => ''		# revision number
-		},
+			:name         => '',		# version control name
+			:rev_id       => '',		# revision number
+            :short_rev_id => ''         # short revision id
+		},        
+        :ci => {
+			:build_number_param_name => 'BUILD_NUMBER',
+            :is_nightly              => true,
+            :build_number            => 0
+		}
     }
     
     begin
+        # TODO: support mercurial and svn
 		build_config[:vcs][:rev_id]	= `git log -1 --pretty=format:%H`.chomp
 		build_config[:vcs][:name] = 'git'
+        build_config[:vcs][:short_rev_id] = build_config[:vcs][:rev_id][0..7]
 	rescue
 	end
+    
+    build_config[:ci][:is_nightly]   = ENV['NIGHTLY'].nil? ? true : Boolean(ENV['NIGHTLY'].downcase)
+    build_config[:ci][:build_number] = ENV[build_config[:ci][:build_number_param_name]] || 0
+    
+    build_config[:version][:full] = "#{build_config[:version][:base]}.#{build_config[:ci][:build_number]}"
+    
+    if(build_config[:ci][:is_nightly])
+        build_config[:version][:long] = "#{build_config[:version][:full]}-nightly-#{build_config[:vcs][:short_rev_id]}"
+    else
+        build_config[:version][:long] = "#{build_config[:version][:full]}-#{build_config[:version][:short_rev_id]}"        
+    end
     
     puts
 	puts build_config if build_config[:log]
 	puts
     puts "     Project Name: #{PROJECT_NAME}"
 	puts "Safe Project Name: #{PROJECT_NAME_SAFE}"
+    puts "          Version: #{build_config[:version][:full]} (#{build_config[:version][:long]})"
     puts "     Base Version: #{build_config[:version][:base]}"
 	puts "        Root Path: #{build_config[:paths][:root]}"
     puts
 	puts "              VCS: #{build_config[:vcs][:name]}"
 	print "     Revision ID: #{build_config[:vcs][:rev_id]}"
-	print "  (#{build_config[:vcs][:rev_id][0..7]})" if build_config[:vcs][:name] == 'git'
+	print "  (#{build_config[:vcs][:short_rev_id]})" if build_config[:vcs][:name] == 'git'
 	puts	
 	puts
     
