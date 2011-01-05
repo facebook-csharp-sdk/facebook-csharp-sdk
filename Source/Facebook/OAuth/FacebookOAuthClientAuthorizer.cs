aecd636cc70fe7d69609a2bb043efb6ed349a864
prabirshrestha <mail@prabir.me>
2011-01-05 16:00:28 +0700

    + get AuthorizationServerUri in IOAuthClientAuthorizer

--- Source/Facebook/OAuth/FacebookOAuthClientAuthorizer.cs
+++ Source/Facebook/OAuth/FacebookOAuthClientAuthorizer.cs
@@ -7,6 +7,7 @@ namespace Facebook.OAuth
         private readonly string clientId;
         private readonly string clientSecret;
         private readonly Uri redirectUri;
+        private readonly Uri authorizationServerUri;
 
         public FacebookOAuthClientAuthorizer()
         {
@@ -17,9 +18,15 @@ namespace Facebook.OAuth
             this.clientId = clientId;
             this.clientSecret = clientSecret;
             this.redirectUri = redirectUri;
+            this.authorizationServerUri = new Uri("https://graph.facebook.com/oauth/");
         }
 
         #region Implementation of IOAuthClientAuthorizer
+
+        public Uri AuthorizationServerUri
+        {
+            get { return this.authorizationServerUri; }
+        }
 
         public string ClientID
         {
--- Source/Facebook/OAuth/IOAuthClientAuthorizer.cs
+++ Source/Facebook/OAuth/IOAuthClientAuthorizer.cs
@@ -4,6 +4,7 @@ namespace Facebook.OAuth
 
     public interface IOAuthClientAuthorizer
     {
+        Uri AuthorizationServerUri { get; }
         string ClientID { get; }
         string ClientSecret { get; }
         Uri RedirectUri { get; }
