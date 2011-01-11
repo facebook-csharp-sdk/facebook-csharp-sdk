6e69997c9fc2dd74326ce8cd266b255aa76acd45
prabirshrestha <mail@prabir.me>
2011-01-11 16:52:11 +0700

    + added CodeContracts for FacebookUtils methods

--- Source/Facebook/FacebookUtils.cs
+++ Source/Facebook/FacebookUtils.cs
@@ -1,6 +1,7 @@
 namespace Facebook
 {
     using System;
+    using System.Diagnostics.Contracts;
     using System.IO;
     using System.Security.Cryptography;
 
@@ -8,6 +9,9 @@ namespace Facebook
     {
         internal static byte[] Base64UrlDecode(string base64UrlSafeString)
         {
+            Contract.Requires(!string.IsNullOrEmpty(base64UrlSafeString));
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+
             base64UrlSafeString = base64UrlSafeString.PadRight(base64UrlSafeString.Length + (4 - base64UrlSafeString.Length % 4) % 4, '=');
             base64UrlSafeString = base64UrlSafeString.Replace('-', '+').Replace('_', '/');
             return Convert.FromBase64String(base64UrlSafeString);
@@ -15,11 +19,18 @@ namespace Facebook
 
         internal static string Base64UrlEncode(byte[] input)
         {
+            Contract.Requires(input != null);
+            Contract.Ensures(!string.IsNullOrEmpty(Contract.Result<string>()));
+
             return Convert.ToBase64String(input).Replace("=", String.Empty).Replace('+', '-').Replace('/', '_');
         }
 
         internal static byte[] ComputeHmacSha256Hash(byte[] data, byte[] key)
         {
+            Contract.Requires(data != null);
+            Contract.Requires(key != null);
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+
             using (var crypto = new HMACSHA256(key))
             {
                 return crypto.ComputeHash(data);
@@ -28,6 +39,11 @@ namespace Facebook
 
         internal static string DecryptAes256CBCNoPadding(byte[] encryptedData, byte[] key, byte[] iv)
         {
+            Contract.Requires(encryptedData != null);
+            Contract.Requires(key != null);
+            Contract.Requires(iv != null);
+            Contract.Ensures(!string.IsNullOrEmpty(Contract.Result<string>()));
+
             string result;
 
             var rijn = new RijndaelManaged
