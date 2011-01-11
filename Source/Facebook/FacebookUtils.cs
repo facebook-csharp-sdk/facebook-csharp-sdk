1468e37728b7f647332b4e0fdb38b3058d20ea98
prabirshrestha <mail@prabir.me>
2011-01-11 16:13:22 +0700

    + FacebookUtils.DecryptAes256CBCNoPadding method

--- Source/Facebook/FacebookUtils.cs
+++ Source/Facebook/FacebookUtils.cs
@@ -1,6 +1,8 @@
 namespace Facebook
 {
     using System;
+    using System.IO;
+    using System.Security.Cryptography;
 
     internal class FacebookUtils
     {
@@ -14,6 +16,46 @@ namespace Facebook
         internal static string Base64UrlEncode(byte[] input)
         {
             return Convert.ToBase64String(input).Replace("=", String.Empty).Replace('+', '-').Replace('/', '_');
+        }
+
+        internal static string DecryptAes256CBCNoPadding(byte[] encryptedData, byte[] key, byte[] iv)
+        {
+            string result;
+
+            var rijn = new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.None,
+                KeySize = 256
+            };
+
+            using (var msDecrypt = new MemoryStream(encryptedData))
+            {
+                using (var decryptor = rijn.CreateDecryptor(key, iv))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var swDecrypt = new StreamReader(csDecrypt))
+                        {
+                            result = swDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            rijn.Clear();
+
+            // remove the last \0\0\0\0\0
+            if (!string.IsNullOrEmpty(result))
+            {
+                var index = result.IndexOf('\0');
+                if (index > 0)
+                {
+                    result = result.Substring(0, index);
+                }
+            }
+
+            return result;
         }
     }
 }
