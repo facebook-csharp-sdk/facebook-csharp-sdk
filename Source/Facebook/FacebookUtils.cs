9c44bfaebe4e5ef7dda68d55323477bcef307714
prabirshrestha <mail@prabir.me>
2011-01-11 15:21:28 +0700

    + FacebookUtils.Base64UrlDecode method

--- Source/Facebook/FacebookUtils.cs
+++ Source/Facebook/FacebookUtils.cs
@@ -1,7 +1,14 @@
 namespace Facebook
 {
+    using System;
+
     internal class FacebookUtils
     {
-        
+        internal static byte[] Base64UrlDecode(string base64UrlSafeString)
+        {
+            base64UrlSafeString = base64UrlSafeString.PadRight(base64UrlSafeString.Length + (4 - base64UrlSafeString.Length % 4) % 4, '=');
+            base64UrlSafeString = base64UrlSafeString.Replace('-', '+').Replace('_', '/');
+            return Convert.FromBase64String(base64UrlSafeString);
+        }
     }
 }
