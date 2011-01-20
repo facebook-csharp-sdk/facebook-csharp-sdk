9214a340df7f991e0ef9fe0adba7bd4c45bce18a
prabirshrestha <mail@prabir.me>
2011-01-20 23:17:44 +0700

    * CSMvc3FacebookApp

--- Samples/CSMvc3FacebookApp/Controllers/HomeController.cs
+++ Samples/CSMvc3FacebookApp/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Facebook;
-using Facebook.Web.Mvc.New;
-using Newtonsoft.Json.Linq;
-
-namespace CSMvc3FacebookApp.Controllers
+﻿namespace CSMvc3FacebookApp.Controllers
 {
+    using System.Web.Mvc;
+    using Facebook;
+    using Facebook.Web.Mvc;
+
     [HandleError]
     public class HomeController : Controller
     {
