6be3148862ceb6c1aabfa05c697a408ebe5aac9b
prabirshrestha <mail@prabir.me>
2011-01-25 01:59:45 +0700

    + new mvc sample

---
+++ Samples/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace MvcApplication1.Controllers
+{
+    using System.Web.Mvc;
+    using Facebook;
+    using Facebook.Web.Mvc;
+
+    [FacebookApp("CSharpSamples")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.ViewData["fbApplications"] = FacebookSdk.Applications;
+
+            return View();
+        }
+
+        [FacebookApp("C# Sample 2")]
+        public ActionResult RegisteredFacebookApps()
+        {
+            return View(FacebookSdk.Applications);
+        }
+
+    }
+}
