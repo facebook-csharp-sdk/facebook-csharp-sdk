186b7f1f2cf1c98ca0fa221d46743d75bdde991f
ntotten <none@none>
2011-02-01 13:04:56 -0500

    - Added getting started pages

--- Samples/CSMvcFacebookApp/Controllers/HomeController.cs
+++ Samples/CSMvcFacebookApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@
     using Facebook;
     using Facebook.Web;
     using Facebook.Web.Mvc;
+    using System.Configuration;
 
     [HandleError]
     public class HomeController : Controller
@@ -11,6 +12,35 @@
         public FacebookSession CurrentSession
         {
             get { return (new CanvasAuthorizer()).Session; }
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // YOU DONT NEED ANY OF THIS IN YOUR APPLICATION
+            // THIS METHOD JUST CHECKS TO SEE IF YOU HAVE SETUP
+            // THE SAMPLE. IF THE SAMPLE IS NOT SETUP YOU WILL
+            // BE SENT TO THE GETTING STARTED PAGE.
+
+            base.OnActionExecuting(filterContext);
+
+            bool isSetup = false;
+            var settings = ConfigurationManager.GetSection("facebookSettings");
+            if (settings != null)
+            {
+                var current = settings as IFacebookApplication;
+                if (current.AppId != "{app id}" && 
+                    current.AppSecret != "{app secret}" &&
+                    current.CanvasUrl != "http://apps.facebook.com/{fix this path}/")
+                {
+                    isSetup = true;
+                }
+            }
+
+            if (!isSetup)
+            {
+                filterContext.Result = View("GettingStarted");
+            }
+
         }
 
         public ActionResult Index()
@@ -27,6 +57,11 @@
             ViewData["Firstname"] = (string)result.first_name;
             ViewData["Lastname"] = (string)result.last_name;
 
+            return View();
+        }
+
+        public ActionResult GettingStarted()
+        {
             return View();
         }
     }
