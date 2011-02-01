ae81b27f0eb9e1066c5801183b42363f40d684f5
prabirshrestha <mail@prabir.me>
2011-02-02 01:56:57 +0700

    + CheckIfFacebookAppIsSetupCorrectly for CSASPNETWebsite

--- Samples/CSASPNETWebsite/Default.aspx.cs
+++ Samples/CSASPNETWebsite/Default.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Facebook;
 
 namespace CSASPNETWebsite
 {
@@ -11,7 +13,32 @@ namespace CSASPNETWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            CheckIfFacebookAppIsSetupCorrectly();
+        }
 
+        private void CheckIfFacebookAppIsSetupCorrectly()
+        {
+            // YOU DONT NEED ANY OF THIS IN YOUR APPLICATION
+            // THIS METHOD JUST CHECKS TO SEE IF YOU HAVE SETUP
+            // THE SAMPLE. IF THE SAMPLE IS NOT SETUP YOU WILL
+            // BE SENT TO THE GETTING STARTED PAGE.
+
+            bool isSetup = false;
+            var settings = ConfigurationManager.GetSection("facebookSettings");
+            if (settings != null)
+            {
+                var current = settings as IFacebookApplication;
+                if (current.AppId != "{app id}" &&
+                    current.AppSecret != "{app secret}")
+                {
+                    isSetup = true;
+                }
+            }
+
+            if (!isSetup)
+            {
+                Response.Redirect("~/GettingStarted.aspx");
+            }
         }
     }
 }
