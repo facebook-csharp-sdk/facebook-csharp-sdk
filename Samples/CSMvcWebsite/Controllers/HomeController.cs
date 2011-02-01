979e20ed16ed86a1e49e4f5448796440f0f65e6a
prabirshrestha <mail@prabir.me>
2011-02-02 02:22:12 +0700

    + GettingStarted CSMvcWebsite

--- Samples/CSMvcWebsite/Controllers/HomeController.cs
+++ Samples/CSMvcWebsite/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -53,6 +54,34 @@ namespace Facebook.Samples.HelloWorld.Controllers
             dynamic result = app.Api("/me/feed", parameters, HttpMethod.Post);
 
             return RedirectToAction("About");
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // YOU DONT NEED ANY OF THIS IN YOUR APPLICATION
+            // THIS METHOD JUST CHECKS TO SEE IF YOU HAVE SETUP
+            // THE SAMPLE. IF THE SAMPLE IS NOT SETUP YOU WILL
+            // BE SENT TO THE GETTING STARTED PAGE.
+
+            base.OnActionExecuting(filterContext);
+
+            bool isSetup = false;
+            var settings = ConfigurationManager.GetSection("facebookSettings");
+            if (settings != null)
+            {
+                var current = settings as IFacebookApplication;
+                if (current.AppId != "{app id}" &&
+                    current.AppSecret != "{app secret}")
+                {
+                    isSetup = true;
+                }
+            }
+
+            if (!isSetup)
+            {
+                filterContext.Result = View("GettingStarted");
+            }
+
         }
     }
 }
