f2c8dba6b4ac810e6116f604a578c5a964685dc1
prabirshrestha <mail@prabir.me>
2011-02-09 22:34:42 +0700

    * updated VERSION file to 5.0.2

--- Source/Facebook.Web/Properties/AssemblyInfo.cs
+++ Source/Facebook.Web/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Facebook C# SDK")]
 [assembly: AssemblyCopyright("Microsoft Public License (Ms-PL)")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("5.0.1.0")]
+[assembly: AssemblyVersion("5.0.2.0")]
 
