7a784ffe621759dd17a8b3ca14b5252ed27fb8a4
prabirshrestha <mail@prabir.me>
2011-03-04 13:53:16 +0545

    + IWebClient

---
+++ Source/Facebook/IWebClient.cs
@@ -0,0 +1,31 @@
+namespace Facebook
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Net;
+
+    internal interface IWebClient
+    {
+        WebHeaderCollection Headers { get; set; }
+
+        NameValueCollection QueryString { get; set; }
+
+        WebHeaderCollection ResponseHeaders { get; set; }
+
+        IWebProxy Proxy { get; set; }
+
+        byte[] DownloadData(Uri address);
+
+        byte[] UploadData(Uri address, string method, byte[] data);
+
+        void DownloadDataAsync(Uri address, object userToken);
+
+        void UploadDataAsync(Uri address, string method, byte[] data, object userToken);
+
+        void CancelAsync();
+
+        event DownloadDataCompletedEventHandler DownloadDataCompleted;
+
+        event UploadDataCompletedEventHandler UploadDataCompleted;
+    }
+}
