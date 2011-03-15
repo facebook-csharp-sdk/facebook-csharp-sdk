7d16f64e49fd948e8a89a5defcd8ec810a5e1059
prabirshrestha <mail@prabir.me>
2011-03-16 01:54:14 +0545

    + updated CSASPNETFacebookApp sample

--- Samples/CSASPNETFacebookApp/Site.master.cs
+++ Samples/CSASPNETFacebookApp/Site.master.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Facebook.Web;
 
 public partial class SiteMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        NavigationMenu.Items[0].NavigateUrl = this.CanvasUrlBuilder.BuildCanvasPageUrl("/Default.aspx").AbsoluteUri;
     }
+
+    protected CanvasUrlBuilder CanvasUrlBuilder = new CanvasUrlBuilder();
 }
