52b2fe5fa9e3ac29a6abb1790dbccd76ba0dc138
prabirshrestha <mail@prabir.me>
2011-04-02 00:10:29 +0545

    + added FQL multi query sample for Facebook.Sample nuget package

--- Build/NuGet/Facebook.Sample/Content/Samples/Facebook/FQL.cs.pp
+++ Build/NuGet/Facebook.Sample/Content/Samples/Facebook/FQL.cs.pp
@@ -9,6 +9,10 @@ namespace $rootnamespace$.Samples.Facebook
         public static void RunSamples(string accessToken)
         {
             SingleQuery(accessToken);
+			
+			Console.WriteLine();
+
+            MultiQuery(accessToken);
         }
 
         public static void SingleQuery(string accessToken)
@@ -40,6 +44,35 @@ namespace $rootnamespace$.Samples.Facebook
                 // Note: make sure to handle this exception.
                 throw;
             }
-        }     
+        }
+
+        public static void MultiQuery(string accessToken)
+        {
+            var query1 = "SELECT uid FROM user WHERE uid=me()";
+            var query2 = "SELECT profile_url FROM user WHERE uid=me()";
+
+            try
+            {
+                var fb = new FacebookClient(accessToken);
+
+                var result = (IList<object>)fb.Query(query1, query2);
+
+                var result0 = ((IDictionary<string, object>)result[0])["fql_result_set"];
+                var result1 = ((IDictionary<string, object>)result[1])["fql_result_set"];
+
+                Console.WriteLine("Query 0 result: {0}", result0);
+                Console.WriteLine();
+                Console.WriteLine("Query 1 result: {0}", result1);
+                Console.WriteLine();
+
+                // Note: This json result is not the orginal json string as returned by Facebook.
+                Console.WriteLine("Json: {0}", result.ToString());
+            }
+            catch (FacebookApiException ex)
+            {
+                // Note: make sure to handle this exception.
+                throw;
+            }
+        }    
 	}
 }
