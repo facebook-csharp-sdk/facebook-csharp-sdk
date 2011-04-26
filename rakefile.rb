7c4b6480eaca22f44dd21c801d0b3a24b8dd1dfb
prabirshrestha <mail@prabir.me>
2011-04-26 23:58:56 +0545

    + updated NuGet.CommandLine to v1.3.20425.372

--- rakefile.rb
+++ rakefile.rb
@@ -115,7 +115,7 @@ task :configure do
     }
    
    build_config[:paths][:packages]  = "#{build_config[:paths][:src]}packages/"
-   build_config[:paths][:nuget]  = "#{build_config[:paths][:packages]}NuGet.CommandLine.1.2.20417.31/Tools/NuGet.exe"
+   build_config[:paths][:nuget]  = "#{build_config[:paths][:packages]}NuGet.CommandLine.1.3.20425.372/Tools/NuGet.exe"
    
    build_config[:paths][:xunit][:x86_console_path]  = "#{build_config[:paths][:tools]}xunit-1.7/xunit.console.clr4.exe"
    
