41c23ab70b633ad486ae7d3defb486be1ef0c222
matthaw <none@none>
2011-06-02 23:52:36 -0700

    Adding license header to interfaces

--- Source/Facebook/IFacebookClient.cs
+++ Source/Facebook/IFacebookClient.cs
@@ -1,8 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// --------------------------------
+// <copyright file="IFacebookClient.cs" company="Thuzi LLC (www.thuzi.com)">
+//     Microsoft Public License (Ms-PL)
+// </copyright>
+// <author>Nathan Totten (ntotten.com), Jim Zimmerman (jimzimmerman.com) and Prabir Shrestha (prabir.me)</author>
+// <license>Released under the terms of the Microsoft Public License (Ms-PL)</license>
+// <website>http://facebooksdk.codeplex.com</website>
+// ---------------------------------
 
 namespace Facebook
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
     /// Represents the Facebook Client.
     /// </summary>
