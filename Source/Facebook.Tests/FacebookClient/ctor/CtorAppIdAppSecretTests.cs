97c2f49a97c965c5bd136e9f7d7daceaa90428d1
prabirshrestha <mail@prabir.me>
2011-07-12 01:51:16 +0545

    + FacebookClient ctor appid appsecret tests

---
+++ Source/Facebook.Tests/FacebookClient/ctor/CtorAppIdAppSecretTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Facebook.Tests.FacebookClient.ctor
+{
+    using Facebook;
+    using Xunit;
+
+    public class CtorAppIdAppSecretTests
+    {
+        [Fact]
+        public void GivenAppIdAndAppSecret_AccessTokenIsSetCorrectly()
+        {
+            var fb = new FacebookClient("12", "45");
+
+            Assert.Equal("12|45", fb.AccessToken);
+        }
+    }
+}
