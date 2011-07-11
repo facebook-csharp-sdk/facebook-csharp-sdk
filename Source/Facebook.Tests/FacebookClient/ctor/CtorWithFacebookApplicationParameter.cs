5f8e1e4cb08bd731f92eb5a25bda3570caf7e79a
prabirshrestha <mail@prabir.me>
2011-07-12 01:44:42 +0545

    + FacebookClient ctor IFacebookApplication tests

---
+++ Source/Facebook.Tests/FacebookClient/ctor/CtorWithFacebookApplicationParameter.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Facebook.Tests.FacebookClient.ctor
+{
+    using Facebook;
+    using Xunit;
+
+    public class CtorWithFacebookApplicationParameter
+    {
+        [Fact]
+        public void GivenNullAccessTokenIsNull()
+        {
+            var fb = new FacebookClient((IFacebookApplication)null);
+
+            Assert.Null(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppIdAndAppSecret_AccessTokenIsNotNull()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId", AppSecret = "appSecret" });
+
+            Assert.NotNull(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppIdAndAppSecret_AccessTokenIsCorrectlyBuilt()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId", AppSecret = "appSecret" });
+
+            Assert.Equal("appId|appSecret", fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppIdButNoAppSecret_AccessTokenIsNull()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId" });
+
+            Assert.Null(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppSecretButNoAppId_AccessTokenIsNull()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId" });
+
+            Assert.Null(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppIdButEmptyAppSecret_AccessTokenIsNull()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId", AppSecret = string.Empty });
+
+            Assert.Null(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAppSecretButEmptyAppId_AccessTokenIsNull()
+        {
+            var fb = new FacebookClient(new DefaultFacebookApplication { AppId = "appId", AppSecret = string.Empty });
+
+            Assert.Null(fb.AccessToken);
+        }
+    }
+}
