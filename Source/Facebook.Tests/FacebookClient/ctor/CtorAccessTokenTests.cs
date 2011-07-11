96e8e151a2861ccc278b2aa75acdff6aab17a5e1
prabirshrestha <mail@prabir.me>
2011-07-12 01:47:59 +0545

    + FacebookClient ctor accessToken tests

---
+++ Source/Facebook.Tests/FacebookClient/ctor/CtorAccessTokenTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Facebook.Tests.FacebookClient.ctor
+{
+    using Facebook;
+    using Xunit;
+
+    public class CtorAccessTokenTests
+    {
+        [Fact]
+        public void GivenAccessToken_AccessTokenIsNotNull()
+        {
+            var fb = new FacebookClient("access_token");
+
+            Assert.NotNull(fb.AccessToken);
+        }
+
+        [Fact]
+        public void GivenAccessToken_AccessTokenIsSetCorrectly()
+        {
+            var fb = new FacebookClient("access_token");
+
+            Assert.Equal("access_token", fb.AccessToken);
+        }
+    }
+}
