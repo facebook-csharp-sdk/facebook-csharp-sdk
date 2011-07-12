3dbb2673b7dec5d8cadd21c0a0d4e85cdf6de58e
prabirshrestha <mail@prabir.me>
2011-07-12 21:11:13 +0545

    + GetAsync exception tests

--- Source/Facebook.Tests/FacebookClient/Get/PathOnly.cs
+++ Source/Facebook.Tests/FacebookClient/Get/PathOnly.cs
@@ -188,6 +188,103 @@ namespace Facebook.Tests.FacebookClient.Get
         }
 
         [Fact]
+        public void Async_GetCompletedErrorIsNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+
+            mockFb.ReturnsJson("{\"id\":\"4\",\"name\":\"Mark Zuckerberg\",\"first_name\":\"Mark\",\"last_name\":\"Zuckerberg\",\"link\":\"http:\\/\\/www.facebook.com\\/zuck\",\"username\":\"zuck\",\"gender\":\"male\",\"locale\":\"en_US\"}",
+                 out mockRequest, out mockResponse);
+
+            var fb = mockFb.Object;
+            Exception error = null;
+
+            TestExtensions.Do(evt => fb.GetCompleted += (o, e) => error = e.Error,
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.Null(error);
+        }
+
+        [Fact]
+        public void Async_GetCompletedGetResultDataIsNotNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+
+            mockFb.ReturnsJson("{\"id\":\"4\",\"name\":\"Mark Zuckerberg\",\"first_name\":\"Mark\",\"last_name\":\"Zuckerberg\",\"link\":\"http:\\/\\/www.facebook.com\\/zuck\",\"username\":\"zuck\",\"gender\":\"male\",\"locale\":\"en_US\"}",
+                 out mockRequest, out mockResponse);
+
+            var fb = mockFb.Object;
+            object result = null;
+
+            TestExtensions.Do(evt => fb.GetCompleted += (o, e) => result = e.GetResultData(),
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Async_GetCompletedGetResultDataIsOfTypeJsonObjectIfObject()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+
+            mockFb.ReturnsJson("{\"id\":\"4\",\"name\":\"Mark Zuckerberg\",\"first_name\":\"Mark\",\"last_name\":\"Zuckerberg\",\"link\":\"http:\\/\\/www.facebook.com\\/zuck\",\"username\":\"zuck\",\"gender\":\"male\",\"locale\":\"en_US\"}",
+                 out mockRequest, out mockResponse);
+
+            var fb = mockFb.Object;
+            object result = null;
+
+            TestExtensions.Do(evt => fb.GetCompleted += (o, e) => result = e.GetResultData(),
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.IsAssignableFrom<IDictionary<string, object>>(result);
+            Assert.IsType<JsonObject>(result);
+        }
+
+        [Fact]
+        public void AsyncWithoutUserState_GetCompletedUserStateIsNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+
+            mockFb.ReturnsJson("{\"id\":\"4\",\"name\":\"Mark Zuckerberg\",\"first_name\":\"Mark\",\"last_name\":\"Zuckerberg\",\"link\":\"http:\\/\\/www.facebook.com\\/zuck\",\"username\":\"zuck\",\"gender\":\"male\",\"locale\":\"en_US\"}",
+                 out mockRequest, out mockResponse);
+
+            var fb = mockFb.Object;
+            object userState = null;
+
+            TestExtensions.Do(evt => fb.GetCompleted += (o, e) => userState = e.UserState,
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.Null(userState);
+        }
+
+        [Fact]
         public void Async_DoesNotCallPostCompletedEvent()
         {
             var mockFb = new Mock<FacebookClient> { CallBase = true };
@@ -269,6 +366,96 @@ namespace Facebook.Tests.FacebookClient.Get
             mockWebException.VerifyGetReponse();
 
             Assert.Equal(1, called);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsNotOpen_GetCompletedErrorIsNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.NoInternetConnection(out mockRequest, out mockWebException);
+
+            Exception error = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 error = e.Error;
+                                                                 evt.Set();
+                                                             };
+                                  },
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+
+            Assert.NotNull(error);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsNotOpen_GetCompletedErrorIsWebExceptionWrapper()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.NoInternetConnection(out mockRequest, out mockWebException);
+
+            Exception error = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 error = e.Error;
+                                                                 evt.Set();
+                                                             };
+                                  },
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+
+            Assert.IsAssignableFrom<WebExceptionWrapper>(error);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsNotOpen_GetCompletedGetResultDataIsNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.NoInternetConnection(out mockRequest, out mockWebException);
+
+            object result = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 result = e.GetResultData();
+                                                                 evt.Set();
+                                                             };
+                                  },
+                              () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+
+            Assert.Null(result);
         }
 
         [Fact]
@@ -361,6 +548,102 @@ namespace Facebook.Tests.FacebookClient.Get
             mockResponse.VerifyGetResponseStream();
 
             Assert.Equal(1, called);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsOpen_GetCompletedErrorIsNotNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.FiddlerNoInternetConnection(out mockRequest, out mockResponse, out mockWebException);
+
+            Exception error = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 error = e.Error;
+                                                                 evt.Set();
+                                                             };
+                                  }
+                              , () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.NotNull(error);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsOpen_GetCompletedErrorIsTypeOfWebExceptionWrapper()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.FiddlerNoInternetConnection(out mockRequest, out mockResponse, out mockWebException);
+
+            Exception error = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 error = e.Error;
+                                                                 evt.Set();
+                                                             };
+                                  }
+                              , () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.IsAssignableFrom<WebExceptionWrapper>(error);
+        }
+
+        [Fact]
+        public void AsyncWhenThereIsNoInternetConnectionAndFiddlerIsOpen_GetCompletedGetResultDataIsNull()
+        {
+            var mockFb = new Mock<FacebookClient> { CallBase = true };
+            Mock<HttpWebRequestWrapper> mockRequest;
+            Mock<HttpWebResponseWrapper> mockResponse;
+            Mock<WebExceptionWrapper> mockWebException;
+
+            mockFb.FiddlerNoInternetConnection(out mockRequest, out mockResponse, out mockWebException);
+
+            object result = null;
+            var fb = mockFb.Object;
+
+            TestExtensions.Do(evt =>
+                                  {
+                                      fb.GetCompleted += (o, e) =>
+                                                             {
+                                                                 result = e.GetResultData();
+                                                                 evt.Set();
+                                                             };
+                                  }
+                              , () => fb.GetAsync("/4"), 5000);
+
+            mockFb.VerifyCreateHttpWebRequest(Times.Once());
+            mockRequest.VerifyBeginGetResponse();
+            mockRequest.VerifyEndGetResponse();
+            mockWebException.VerifyGetReponse();
+            mockResponse.VerifyGetResponseStream();
+
+            Assert.Null(result);
         }
 
         [Fact]
