4fcf214a7dce29a840c6dd6d4d3a0600ca266594
prabirshrestha <mail@prabir.me>
2011-07-14 00:37:02 +0545

    + added more FacebookClient tests to silverlight tests

--- Source/Facebook.Tests/FacebookClient/GetApiUrl/Fakes/FakeFacebookClient.cs
+++ Source/Facebook.Tests/FacebookClient/GetApiUrl/Fakes/FakeFacebookClient.cs
@@ -7,11 +7,15 @@ namespace Facebook.Tests.FacebookClient.GetApiUrl.Fakes
     class FakeFacebookClient : FacebookClient
     {
         #region not implemented
+
+#if !SILVERLIGHT
         
         internal protected override object Api(string path, IDictionary<string, object> parameters, HttpMethod httpMethod, Type resultType)
         {
             throw new NotImplementedException();
         }
+
+#endif
 
         internal protected override void ApiAsync(string path, IDictionary<string, object> parameters, HttpMethod httpMethod, object userToken)
         {
@@ -19,11 +23,6 @@ namespace Facebook.Tests.FacebookClient.GetApiUrl.Fakes
         }
 
         #endregion
-
-        protected override Dictionary<string, Uri> DomainMaps
-        {
-            get { return base.DomainMaps; }
-        }
 
         public new System.Uri GetApiUrl(string method)
         {
