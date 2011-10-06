1a3806c5b2e3e32f7fa610c36ffee1680ebefd94
prabirshrestha <mail@prabir.me>
2011-10-06 23:31:15 +0545

    - removed code contracts from FacebookBatchParameter

---
+++ Source/Facebook.Tests/FacebookApplication/FacebookApplicationTests.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Facebook.Tests.FacebookApplication
+{
+    using System;
+    using Facebook;
+    using Xunit;
+
+    public class SetApplicationTests
+    {
+        [Fact]
+        public void GivenIFacebookApplicationAsNullThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => FacebookApplication.SetApplication((IFacebookApplication)null));
+        }
+
+        [Fact]
+        public void GivenFuncIFacebookApplicationAsNullThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => FacebookApplication.SetApplication((Func<IFacebookApplication>)null));
+        }
+    }
+}
---
+++ Source/Facebook.Tests/FacebookBatchParameter/FacebookBatchParameterTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Facebook.Tests.FacebookBatchParameter
+{
+    using System;
+    using Facebook;
+    using Xunit;
+
+    public class FacebookBatchParameterTests
+    {
+        [Fact]
+        public virtual void GivenNullFqlInQueryThrowsArgumentNullException()
+        {
+            var bp = new FacebookBatchParameter();
+
+            Assert.Throws<ArgumentNullException>(() => bp.Query((string)null));
+        }
+
+        [Fact]
+        public virtual void GivenEmptyFqlInQueryThrowsArgumentNullException()
+        {
+            var bp = new FacebookBatchParameter();
+
+            Assert.Throws<ArgumentNullException>(() => bp.Query(string.Empty));
+        }
+
+        [Fact]
+        public virtual void GivenNullFqlInMultiQueryThrowsArgumentNullException()
+        {
+            var bp = new FacebookBatchParameter();
+
+            Assert.Throws<ArgumentNullException>(() => bp.Query((string[])null));
+        }
+
+        [Fact]
+        public virtual void GivenEmptyFqlInMultiQueryThrowsArgumentNullException()
+        {
+            var bp = new FacebookBatchParameter();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => bp.Query(new string[] { }));
+        }
+    }
+}
