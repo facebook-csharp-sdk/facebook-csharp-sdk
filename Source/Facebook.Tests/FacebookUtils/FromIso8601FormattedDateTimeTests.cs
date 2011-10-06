32ef1009d7cfb011a5e39a8dedb2e67708f9987c
prabirshrestha <mail@prabir.me>
2011-10-06 23:07:12 +0545

    - removed code contracts from DateTimeConvertor

---
+++ Source/Facebook.Tests/FacebookUtils/FromIso8601FormattedDateTimeTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Facebook.Tests.FacebookUtils
+{
+    using System;
+    using Xunit;
+
+    public class FromIso8601FormattedDateTimeTests
+    {
+        [Fact]
+        public void GivenNullThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => DateTimeConvertor.FromIso8601FormattedDateTime(null));
+        }
+
+        [Fact]
+        public void GivenEmptyThrowsArgumnetNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => DateTimeConvertor.FromIso8601FormattedDateTime(string.Empty));
+        }
+    }
+}
