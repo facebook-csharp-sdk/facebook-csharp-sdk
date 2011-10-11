8fdc3fd6c6cfc716c326753c5ae44a42ab60aefa
prabirshrestha <mail@prabir.me>
2011-10-11 19:16:11 +0545

    - removed code contracts

--- Build/NuGet/Facebook/tools/Install.ps1
+++
@@ -1,2 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-    $project.Object.References | Where-Object { $_.Name -eq 'Facebook.Contracts' } | ForEach-Object { $_.Remove() }
--- Build/NuGet/FacebookWeb/tools/Install.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-	$project.Object.References.Add("System.Web")
-    $project.Object.References | Where-Object { $_.Name -eq 'Facebook.Web.Contracts' } | ForEach-Object { $_.Remove() }
