17c12f33d642bd71d349ec4edabb42bdc7804f10
prabirshrestha <mail@prabir.me>
2011-10-13 01:54:51 +0545

    + added CS-AspNetMvc3-WithoutJsSdk.sln sample demonstrating the use of oauth authorization, with FormsAuthentication and without using the Facebook JS SDK. (also includes prevention of Cross Site Request Forgery - CSRF attacks.)

---
+++ Samples/CS-AspNetMvc3-WithoutJsSdk/Models/InMemoryUserStore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+
+namespace CS_AspNetMvc3_WithoutJsSdk.Models
+{
+    public class InMemoryUserStore
+    {
+        private static readonly ConcurrentBag<FacebookUser> Users = new ConcurrentBag<FacebookUser>();
+
+        public static void AddOrUpdate(FacebookUser user)
+        {
+            var ux = Users.SingleOrDefault(u => u.FacebookId == user.FacebookId);
+
+            if (ux == null)
+            {
+                Users.Add(user);
+            }
+            else
+            {
+                ux.AccessToken = user.AccessToken;
+                ux.FacebookId = user.FacebookId;
+                ux.Name = user.Name;
+                ux.Expires = user.Expires;
+            }
+        }
+
+        public static FacebookUser Get(string facebookId)
+        {
+            return Users.SingleOrDefault(u => u.FacebookId == facebookId);
+        }
+    }
+}
