b29596c70a820099dc788a8ab1e0c37de2bd326c
prabirshrestha <mail@prabir.me>
2011-10-27 18:54:01 +0545

    + updated version to v5.3.1

--- Source/Facebook/Properties/AssemblyInfo.cs
+++ Source/Facebook/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Facebook C# SDK")]
 [assembly: AssemblyCopyright("Microsoft Public License (Ms-PL)")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("5.3.0.0")]
+[assembly: AssemblyVersion("5.3.1.0")]
 
