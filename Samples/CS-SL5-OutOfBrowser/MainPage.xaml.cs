1be66bfc01c674fedd2fdc37d28fc88fcd2513f6
prabirshrestha <mail@prabir.me>
2011-12-11 13:06:29 +0700

    + empty CS-SL5-OutOfBrowser sample

---
+++ Samples/CS-SL5-OutOfBrowser/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CS_SL5_OutOfBrowser
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
