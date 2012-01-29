098d9de8a0a20baeddd68f1247d40dfb27f49295
prabirshrestha <mail@prabir.me>
2012-01-29 14:11:50 -0500

    - removed NuGet packages for FacebookWeb, FacebookWebMvc and Facebook.JavascriptMvcWebsite (fxi #56)

--- Build/NuGet/FacebookWebMvc/tools/Install.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-	$project.Object.References.Add("System.Web.Mvc")
-    $project.Object.References | Where-Object { $_.Name -eq 'Facebook.Web.Mvc.Contracts' } | ForEach-Object { $_.Remove() }
