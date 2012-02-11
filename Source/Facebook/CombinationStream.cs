ab25fbb03d14842c2a5077c62f956cd6139ee42a
prabirshrestha <mail@prabir.me>
2012-02-11 11:47:21 -0500

    + updated CombinationStream to v2.0.1

--- Source/Facebook/CombinationStream.cs
+++ Source/Facebook/CombinationStream.cs
@@ -1,19 +1,24 @@
+//-----------------------------------------------------------------------
+// <copyright file="CombinationStream.cs" company="Prabir Shrestha">
+//    Copyright 2011 Prabir Shrestha
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+// <author>Prabir Shrestha (prabir.me)</author>
+// <website>https://github.com/facebook-csharp-sdk/CombinationStream</website>
+//-----------------------------------------------------------------------
+
 /*
  * Install-Package CombinationStream
- * 
- * https://github.com/prabirshrestha/CombinationStream
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- *    
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  * 
  */
 
