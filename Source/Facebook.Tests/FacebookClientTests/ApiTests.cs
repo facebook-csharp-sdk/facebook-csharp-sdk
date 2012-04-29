ed608f47292affa6d1d8e7494d4fa64dd76021a0
prabirshrestha <mail@prabir.me>
2012-04-29 13:53:18 -0400

    format json-strings test

--- Source/Facebook.Tests/FacebookClientTests/ApiTests.cs
+++ Source/Facebook.Tests/FacebookClientTests/ApiTests.cs
@@ -230,5 +230,40 @@
                 Assert.Equal("Mark Zuckerberg", result.name);
             }
         }
+
+        public class WhenPropertyContainsFormat
+        {
+            [Fact]
+            public void OverrideFormatWithJsonString()
+            {
+                var fb = new FacebookClient();
+
+                FakeHttpWebRequestWrapper fakeRequest = null;
+                FakeHttpWebResponseWrapper fakeResponse = null;
+
+                fb.HttpWebRequestFactory =
+                    uri => fakeRequest =
+                           new FakeHttpWebRequestWrapper()
+                               .WithRequestUri(uri)
+                               .FakeResponse()
+                               .WithResponseStreamAs(
+                                   "{\"id\":\"4\",\"name\":\"Mark Zuckerberg\",\"first_name\":\"Mark\",\"last_name\":\"Zuckerberg\",\"link\":\"http:\\/\\/www.facebook.com\\/zuck\",\"username\":\"zuck\",\"gender\":\"male\",\"locale\":\"en_US\"}")
+                               .WithContentType("text/javascript; charset=UTF-8")
+                               .WithStatusCode(200)
+                               .GetFakeHttpWebRequestWrapper();
+
+                var parameters = new Dictionary<string, object>();
+                parameters["format"] = "xml";
+                dynamic result = fb.Get("4", parameters);
+
+                Assert.Equal("GET", fakeRequest.Method);
+                Assert.Equal("https://graph.facebook.com/4?format=json-strings", fakeRequest.RequestUri.AbsoluteUri);
+                Assert.True(fakeRequest.ContentLength == 0);
+
+                Assert.IsAssignableFrom<IDictionary<string, object>>(result);
+                Assert.Equal("4", result.id);
+                Assert.Equal("Mark Zuckerberg", result.name);
+            }
+        }
     }
 }
