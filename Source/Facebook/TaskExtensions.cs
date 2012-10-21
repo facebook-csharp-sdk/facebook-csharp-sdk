0aa2048a55564819273b0779734a191452da3604
prabirshrestha <mail@prabir.me>
2012-10-21 17:28:48 -0400

    aded Task<T2> Then<T1, T2>(this Task<T1> first, Func<T1, T2> next)

--- Source/Facebook/TaskExtensions.cs
+++ Source/Facebook/TaskExtensions.cs
@@ -19,8 +19,35 @@
 
 namespace Facebook
 {
+    using System;
+    using System.Threading.Tasks;
+
     static class TaskExtensions
     {
-         
+        public static Task<T2> Then<T1, T2>(this Task<T1> first, Func<T1, T2> next)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (next == null) throw new ArgumentNullException("next");
+
+            var tcs = new TaskCompletionSource<T2>();
+            first.ContinueWith(delegate
+            {
+                if (first.IsFaulted) tcs.TrySetException(first.Exception.InnerExceptions);
+                else if (first.IsCanceled) tcs.TrySetCanceled();
+                else
+                {
+                    try
+                    {
+                        var result = next(first.Result);
+                        tcs.TrySetResult(result);
+                    }
+                    catch (Exception ex)
+                    {
+                        tcs.TrySetException(ex);
+                    }
+                }
+            });
+            return tcs.Task;
+        }
     }
 }
