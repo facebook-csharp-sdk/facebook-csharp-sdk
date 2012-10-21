565dbf934d6e81fc6871421c5b439a7bb8553f35
prabirshrestha <mail@prabir.me>
2012-10-21 17:31:20 -0400

    Task<TResult> GetTaskAsync<TResult>( object parameters)

--- Source/Facebook/FacebookClient.Async.Tasks.cs
+++ Source/Facebook/FacebookClient.Async.Tasks.cs
@@ -265,6 +265,17 @@ namespace Facebook
         }
 
         /// <summary>
+        /// Makes an asynchronous GET request to the Facebook server.
+        /// </summary>
+        /// <param name="parameters">The parameters</param>
+        /// <typeparam name="TResult">The result type.</typeparam>
+        /// <returns>The json result task.</returns>
+        public virtual Task<TResult> GetTaskAsync<TResult>( object parameters)
+        {
+            return GetTaskAsync<TResult>(null, parameters);
+        }
+
+        /// <summary>
         /// Makes an asynchronous POST request to the Facebook server.
         /// </summary>
         /// <param name="parameters">The parameters</param>
